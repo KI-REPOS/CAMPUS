1393add8c70bf24187078dc969e38f0970823f12
Kiran Surya Kumar K <kiransuryakumar.k@gmail.com>
2025-11-29 12:30:06 +0530

    end point change

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://campus-ly0c.onrender.com',
+  apiBaseUrl: 'https://campus-ly0c.onrender.com/api',
   socketUrl: 'https://campus-ly0c.onrender.com'
 };
